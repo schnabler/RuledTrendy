7684694b6ecd12bc6df0d75871767bd7d4275d7d
Tobias Dorner <t.dorner@chi-deutschland.com>
2016-04-15 12:48:11 +0200

    Initial Commit

---
+++ RuledTrendy/RuledTrendy.h
@@ -0,0 +1,19 @@
+//
+//  RuledTrendy.h
+//  RuledTrendy
+//
+//  Created by Tobias Dorner on 15.04.16.
+//  Copyright © 2016 BCKTWN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RuledTrendy.
+FOUNDATION_EXPORT double RuledTrendyVersionNumber;
+
+//! Project version string for RuledTrendy.
+FOUNDATION_EXPORT const unsigned char RuledTrendyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RuledTrendy/PublicHeader.h>
+
+
